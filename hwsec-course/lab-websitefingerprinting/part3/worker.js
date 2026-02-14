56ed1be58a0f4ebcd96832d1e2e1d64063a8158c
kudzo <andrerastrelly14@gmail.com>
2026-02-13 20:40:48 -0500

    feat: deleted all commented code

--- hwsec-course/lab-websitefingerprinting/part3/worker.js
+++ hwsec-course/lab-websitefingerprinting/part3/worker.js
@@ -1,6 +1,6 @@
 // Number of sweep counts
 // TODO (Exercise 3-1): Choose an appropriate value!
-let P = 1000;
+let P = 0.6;
 
 // Number of elements in your trace
 let K = (5 * 1000) / P;
@@ -25,26 +25,17 @@ function record() {
     const LINE_SIZE = 16;
     let numLines = 10000;
 
-    // const M = new Array(LINE_SIZE * numLines).fill(-1); // buffer
-
     let traceIndex = 0;
-    // let sum = 0;
+
     while (traceIndex < K) {
         let counter = 0;
         let traceBegin = performance.now();
-        // let j = 0;
+
         do {
             counter++;
-            // for (let i = 0; i < numLines; i++) {
-            //     // let val = M[i * LINE_SIZE + j * LINE_SIZE];
-            //     // sum += val;
-            // }
-            // j++;
         } while (performance.now() - traceBegin < P);
         T[traceIndex] = counter;
-        //console.log(T[traceIndex]);
         traceIndex++;
-        // j = 0;
         counter = 0;
     }
 
