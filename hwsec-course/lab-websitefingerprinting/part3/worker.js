e97af9c7566247552f07f0b0e36885a99be7e284
kudzo <andrerastrelly14@gmail.com>
2026-02-13 19:59:22 -0500

    feat: all work from parts 2 and 3

--- hwsec-course/lab-websitefingerprinting/part3/worker.js
+++ hwsec-course/lab-websitefingerprinting/part3/worker.js
@@ -3,7 +3,7 @@
 let P = 1000;
 
 // Number of elements in your trace
-let K = 5 * 1000 / P; 
+let K = (5 * 1000) / P;
 
 // Array of length K with your trace's values
 let T;
@@ -12,24 +12,49 @@ let T;
 let start;
 
 function record() {
-  // Create empty array for saving trace values
-  T = new Array(K);
+    // Create empty array for saving trace values
+    T = new Array(Math.floor(K));
 
-  // Fill array with -1 so we can be sure memory is allocated
-  T.fill(-1, 0, T.length);
+    // Fill array with -1 so we can be sure memory is allocated
+    T.fill(-1, 0, T.length);
 
-  // Save start timestamp
-  start = performance.now();
+    // Save start timestamp
+    start = performance.now();
 
-  // TODO (Exercise 3-1): Record data for 5 seconds and save values to T.
+    // TODO (Exercise 2-1): Record data for 5 seconds and save values to T.
+    const LINE_SIZE = 16;
+    let numLines = 10000;
 
-  // Once done recording, send result to main thread
-  postMessage(JSON.stringify(T));
+    // const M = new Array(LINE_SIZE * numLines).fill(-1); // buffer
+
+    let traceIndex = 0;
+    // let sum = 0;
+    while (traceIndex < K) {
+        let counter = 0;
+        let traceBegin = performance.now();
+        // let j = 0;
+        do {
+            counter++;
+            // for (let i = 0; i < numLines; i++) {
+            //     // let val = M[i * LINE_SIZE + j * LINE_SIZE];
+            //     // sum += val;
+            // }
+            // j++;
+        } while (performance.now() - traceBegin < P);
+        T[traceIndex] = counter;
+        //console.log(T[traceIndex]);
+        traceIndex++;
+        // j = 0;
+        counter = 0;
+    }
+
+    // Once done recording, send result to main thread
+    postMessage(JSON.stringify(T));
 }
 
 // DO NOT MODIFY BELOW THIS LINE -- PROVIDED BY COURSE STAFF
 self.onmessage = (e) => {
-  if (e.data.type === "start") {
-    setTimeout(record, 0);
-  }
+    if (e.data.type === "start") {
+        setTimeout(record, 0);
+    }
 };
